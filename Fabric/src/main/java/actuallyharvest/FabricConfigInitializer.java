5e1a358035604e955a531f2ea2ccb196c088027c
Wendall Cada <wendallc@83864.com>
2024-08-13 12:02:44 -0700

    Defer config init. Fixes #3

--- Fabric/src/main/java/actuallyharvest/FabricConfigInitializer.java
+++ Fabric/src/main/java/actuallyharvest/FabricConfigInitializer.java
@@ -4,14 +4,17 @@ import com.illusivesoulworks.spectrelib.config.SpectreConfigInitializer;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
+import actuallyharvest.config.ConfigHandler;
+
 public class FabricConfigInitializer implements SpectreConfigInitializer {
 
     @Override
     public void onInitializeConfig() {
+        ActuallyHarvest.init();
+
         ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-            ActuallyHarvest.init();
+            ConfigHandler.init();
         });
     }
 
 }
-
