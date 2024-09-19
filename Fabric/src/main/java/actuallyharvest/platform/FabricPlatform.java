3254c7f5a5796388a120e93824b43c281844cfaf
Wendall Cada <wendallc@83864.com>
2024-09-19 12:30:21 -0700

    File cleanup.

--- Fabric/src/main/java/actuallyharvest/platform/FabricPlatform.java
+++ Fabric/src/main/java/actuallyharvest/platform/FabricPlatform.java
@@ -4,8 +4,6 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 
 import actuallyharvest.platform.services.IPlatform;
-import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.block.state.BlockState;
 
 public class FabricPlatform implements IPlatform {
 
