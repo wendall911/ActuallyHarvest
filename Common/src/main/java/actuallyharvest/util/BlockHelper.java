072db660c8bbf87e9d94213f3d8e1b98429da1f3
Wendall Cada <wendallc@83864.com>
2025-03-12 09:50:32 -0700

    Add ability to blacklist mods or specific crops. Reesolves #14

--- Common/src/main/java/actuallyharvest/util/BlockHelper.java
+++ Common/src/main/java/actuallyharvest/util/BlockHelper.java
@@ -10,6 +10,7 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
 import net.minecraft.commands.arguments.blocks.BlockStateParser;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.HoneycombItem;
 import net.minecraft.world.item.ItemStack;
@@ -30,11 +31,15 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty;
 public class BlockHelper {
 
     public static boolean isVanilla(Block block) {
-        return BuiltInRegistries.BLOCK.getKey(block).getNamespace().equals("minecraft");
+        return getBlockId(block).getNamespace().equals("minecraft");
     }
 
     public static boolean isBottomBlock(Block block) {
-        return BuiltInRegistries.BLOCK.getKey(block).getPath().contains("_bottom");
+        return getBlockId(block).getPath().contains("_bottom");
+    }
+
+    public static ResourceLocation getBlockId(Block block) {
+        return BuiltInRegistries.BLOCK.getKey(block);
     }
 
     public static String[] parseBlockString(String blockString) {
