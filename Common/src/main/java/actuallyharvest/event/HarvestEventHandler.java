73d6dcc854c9dd532c28b79a6b9dbb950ae6558f
Wendall Cada <wendallc@83864.com>
2025-03-12 11:27:43 -0700

    Add setting for using xp drop range instead of chance and amount. Implements #17

--- Common/src/main/java/actuallyharvest/event/HarvestEventHandler.java
+++ Common/src/main/java/actuallyharvest/event/HarvestEventHandler.java
@@ -9,6 +9,8 @@
  */
 package actuallyharvest.event;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.apache.commons.lang3.mutable.MutableBoolean;
 
 import org.jetbrains.annotations.Nullable;
@@ -173,7 +175,14 @@ public class HarvestEventHandler {
             boolean dropXp = entity instanceof Player;
             blockState.spawnAfterBreak(serverLevel, pos, copy, dropXp);
 
-            if (dropXp && ActuallyHarvest.RANDOM.nextInt(100) + 1 <= ConfigHandler.Common.xpFromHarvestChance()) {
+            if (dropXp && ConfigHandler.Common.xpFromHarvestUseRange()) {
+                int xp = ThreadLocalRandom.current().nextInt(ConfigHandler.Common.xpFromHarvestRangeAmount().getLeft(), ConfigHandler.Common.xpFromHarvestRangeAmount().getRight() + 1);
+
+                if (xp > 0) {
+                    ExperienceOrb.award(serverLevel, Vec3.atCenterOf(pos), xp);
+                }
+            }
+            else if (dropXp && ActuallyHarvest.RANDOM.nextInt(100) + 1 <= ConfigHandler.Common.xpFromHarvestChance()) {
                 ExperienceOrb.award(serverLevel, Vec3.atCenterOf(pos), ConfigHandler.Common.xpFromHarvestAmount());
             }
 
